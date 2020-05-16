1cc09bf6e7a9ff1c56c1fff9d7038d30bdbf1471
Joaquin Guardado <joaquin_guardado@hotmail.com>
2020-05-16 20:34:27 +0200

    added static files middleware

--- reactor/settings.py
+++ reactor/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig'
 ]
