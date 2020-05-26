f24a605d49437a83d35126edd263945a5404c239
Joaquin Guardado <joaquin_guardado@hotmail.com>
2020-05-26 16:38:40 +0200

    Added blog detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 # blog/urls.py
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
+
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -9,3 +9,8 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
