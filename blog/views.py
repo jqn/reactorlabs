919c4c30591626b7106ab6a2e551e0dc3acdd970
Joaquin Guardado <jqn@Joaquins-MacBook-Pro.local>
2020-08-07 19:03:45 +0200

    Added back updated dashboard

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import  get_object_or_404
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+    )
 from django.urls import reverse_lazy
 
 from .models import Post, Category
