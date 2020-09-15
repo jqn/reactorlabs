78ce283c51b7dca30df027083aaf4f2a6f7b9447
jqn <jqngl@icloud.com>
2020-09-15 17:17:35 +0200

    More forms styling

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 # users/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import gettext_lazy as _
+
 
 from .models import CustomUser
 
@@ -8,10 +10,17 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
 
     editor = forms.BooleanField(required=False)
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password Confirmation',
+                                widget=forms.PasswordInput)
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('username', 'email', 'editor',)
+        fields = ('username', 'email', 'editor', 'password1', 'password2',)
+        help_texts = {
+            'username': None,
+        }
 
 
 class CustomUserChangeForm(UserChangeForm):
