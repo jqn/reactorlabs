2d163e452773787a328358582ee18600e251f847
jqn <jqngl@icloud.com>
2021-01-11 16:36:16 +0100

    Added production wsgi to pass apache env variables

--- reactor/wsgi.py
+++ reactor/wsgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+_application = get_wsgi_application()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reactor.settings')
 
-application = get_wsgi_application()
+env_variables_to_pass = ['SECRET_KEY', 'DB_NAME',
+                         'DB_USERNAME', 'DB_PASSWORD',
+                         'DB_HOSTNAME', 'DB_PORT']
+
+
+def application(environ, start_response):
+    # pass the WSGI environment variables on through to os.environ
+    for var in env_variables_to_pass:
+        os.environ[var] = environ.get(var, '')
+    return _application(environ, start_response)
