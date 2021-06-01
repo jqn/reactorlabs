a3a52f2ef5f8467bdb5b3d193d1f9143223e45e3
jqn <joaquin_guardado@icloud.com>
2021-06-01 18:07:37 +0200

    Work in progress debugging registration emails

--- reactor/urls.py
+++ reactor/urls.py
@@ -28,8 +28,6 @@ urlpatterns = [
     path('', include('captures.urls')),
     path('', include('pages.urls')),
     path('blog/', include('blog.urls')),
-    # path('users/', include('users.urls')),
-    # path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
